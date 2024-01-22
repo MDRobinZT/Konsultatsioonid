db1f6b6d6cd0795ffe5fa8dd6dee1c661bc8e52c
MDRobinZT <127193041+MDRobinZT@users.noreply.github.com>
2024-01-22 13:28:44 +0200

    Delete Konsultatsioonid directory

--- Konsultatsioonid/conf.php
+++
@@ -1,7 +0,0 @@
-<?php
-$serverinimi="d125314.mysql.zonevs.eu";
-$kasutajanimi="d125314_robin";
-$parool="Bababooey!";
-$andmebaas="d125314_robinbaas";
-$yhendus=new mysqli($serverinimi, $kasutajanimi, $parool, $andmebaas);
-$yhendus->set_charset("UTF8");
